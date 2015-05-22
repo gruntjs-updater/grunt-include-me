c47353df5965ab7731e966792c16e32cebc67194
darkylin <darkylin1989@gmail.com>
2015-05-22 13:20:13 +0800

    联调

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,11 +22,11 @@ module.exports = function(grunt) {
 
     // Configuration to be run (and then tested).
     include_me: {
-      options: {
+        dev:{
+            src: ['<%=path.dev%>/*.hbs'],
+            dist:'<%=path.dev%>/dist'
+        }
 
-      },
-        src: ['<%=path.dev%>/*.hbs'],
-        dist:'<%=path.dev%>/dist'
     }
 
   });
@@ -34,16 +34,8 @@ module.exports = function(grunt) {
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'include_me', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', [ 'include_me']);
 
 };
--- tasks/include_me.js
+++ tasks/include_me.js
@@ -13,12 +13,44 @@ module.exports = function (grunt) {
     // Please see the Grunt documentation for more information regarding task
     // creation: http://gruntjs.com/creating-tasks
 
-    grunt.registerMultiTask('include_me', 'grunt plugin to make including file easier', function () {
-        // Merge task-specific and/or target-specific options with these defaults.
-        var options = this.options({});
 
+    grunt.registerMultiTask('include_me', 'grunt plugin to make including file easier', function () {
+        var options = this.options({
+            directive:'@include'
+        });
+        // Merge task-specific and/or target-specific options with these defaults.
+        var gruntDone = this.async();
+
+        var fs = require('fs'),
+            split=require('split'),
+            through = require('through2'),
+            path = require('path');
+
+        var fileCount = 0;
+        var INCLUDE_REP=new RegExp('^\\s*'+options.directive+'\\s*([\\w/]+)\\s*$');
+        var cache = {};
         this.files.forEach(function (f) {
-            console.dir(f);
+            f.src.forEach(function(src){
+                fileCount++;
+                fs.createReadStream(src).pipe(split()).pipe(through(function(buffer,encoding,next){
+                    var line = buffer.toString(),
+                        includeFile=line.match(INCLUDE_REP);
+                    if(includeFile!==null){
+                        var fileName = includeFile[1];
+                        path.dirname(src)
+                        this.push(null);
+                    }else{
+                        this.push(buffer);
+                    }
+                    next();
+                },function(done){
+                    done();
+                    fileCount--;
+                    if(fileCount==0){
+                        gruntDone();
+                    }
+                }));
+            })
         });
     });
 
